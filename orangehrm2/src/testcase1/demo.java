554162ac8369d2210fd433f59cd8e60f19ce95e4
NAGAPURI SANDEEP <nagapuri.sandeep@shivila.com>
2023-08-14 14:31:23 +0530

    commit three

--- orangehrm2/src/testcase1/demo.java
+++ orangehrm2/src/testcase1/demo.java
@@ -5,6 +5,7 @@ public class demo {
 	public static void main(String[] args) {
 		System.out.println("this is first test case");
 		System.out.println("this is second test case");
+		System.out.println("this is third  test case");
 		
 
 	}
